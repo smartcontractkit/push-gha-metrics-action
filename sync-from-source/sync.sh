3231d5010de5b49e1d1bef34039349245b2cd797
Erik Burton <erikburt@users.noreply.github.com>
2023-11-03 15:01:07 -0700

    feat: update release process (#52)

--- sync-from-source/sync.sh
+++ sync-from-source/sync.sh
@@ -5,30 +5,37 @@ set -e
 # gh cli ^2.15.0 https://github.com/cli/cli/releases/tag/v2.15.0
 # jq ^1.6 https://stedolan.github.io/jq/
 
-repo_name=push-gha-metrics-action-source
-repo_location=smartcontractkit/$repo_name
-gitRoot=$(git rev-parse --show-toplevel)
-
+# Echos (green colored) message to standard error
 msg() {
   echo -e "\033[32m$1\033[39m" >&2
 }
 
-echo "Getting latest release for tag for $repo_name"
-release=$(gh release view -R $repo_location --json 'tagName,body')
-tag=$(echo "$release" | jq -r '.tagName')
-
-echo "Getting release $tag for $repo_name"
-
 # Function to download a file from GitHub API
 download_file() {
   local path="$1"
+  msg "Downloading $path@$tag"
 
-  msg "Downloading $path"
   gh api -XGET \
     -H "Accept: application/vnd.github.raw" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
-    /repos/smartcontractkit/push-gha-metrics-action-source/contents/"$path" -F ref="$tag" >"$path"
+    "/repos/$repo_location/contents/$path" -F ref="$tag" > "$path"
 }
+
+repo_name=push-gha-metrics-action-source
+repo_location=smartcontractkit/$repo_name
+gitRoot=$(git rev-parse --show-toplevel)
+
+# Determine if this sync is for a release or a snapshot
+if [[ -n "$1" ]] && [[ "$1" == "--snapshot" ]]; then
+  # Download from main branch directly for snapshots
+  echo "Snapshot sync - will download changes from main branch"
+  tag=main
+else
+  # Download from the most recent tagged release
+  echo "Relase sync - getting latest release for $repo_location"
+  release=$(gh release view -R $repo_location --json 'tagName,body')
+  tag=$(echo "$release" | jq -r '.tagName')
+fi
 
 pushd "$gitRoot" >/dev/null
 
